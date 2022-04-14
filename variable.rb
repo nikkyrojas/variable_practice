fd16165cf135088305110a647cec710f83c0b225
Nikky Rojas <nikkyrojas@Nikkys-MacBook-Pro.local>
2022-04-13 20:25:00 -0600

    add 3 comments

--- variable.rb
+++ variable.rb
@@ -2,6 +2,7 @@ name = "Nikky"
 fav_food = "barbacoa"
 fav_vacay = "florida"
 
+#assigning integers to a variable
 age = 25
 kids = 2
 dogs = 2
@@ -10,9 +11,11 @@ milk_price = 8.50
 chicken_price = 7.50
 eggs_price = 1.50
 
+#boolean example its is true I am a adult it false i am a boy
 adult = true
 boy = false
 
+#reassigned a variable
 name = "Noemi"
 fav_food = "pizza"
 fav_vacay = "beach"
